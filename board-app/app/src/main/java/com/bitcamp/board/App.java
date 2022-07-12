3c30265f7d2563dccced04e866de9f335bc9fa05
zzllang <wmfps9@naver.com>
2022-07-12 17:21:03 +0900

    Exam_20220712

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -6,26 +6,37 @@ package com.bitcamp.board;
 
 public class App {
 
-  public static void main(String[] args) {
-    System.out.println("[게시판 애플리케이션!]");
+  public static void main(String[] args) { //진입점
+    System.out.println("[게시판 애플리케이션]");
     System.out.println();
     System.out.println("환영합니다!");
     System.out.println();
 
-    java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
+    java.util.Scanner keyboardInput = new java.util.Scanner(System.in); //System이라는 도구 안에 in(사용자가 키보드 입력한 걸 읽는) 도구를 사용
+
+    String title = "";
+    String content = "";
+    String writer = "";
+    String password = "";
+
+    // String[] titles = new Stirng[1000];
+
     while (true) {
       System.out.println("메뉴:");
-      System.out.println("[1: 게시글 목록]");
-      System.out.println("[2: 게시판 상세보기]");
+      System.out.println("[  1: 게시글 목록]");
+      System.out.println("[  2: 게시글 상세보기]");
+      System.out.println("[  3: 게시글 등록]");
       System.out.println();
+      System.out.println("메뉴를 선택하세요 [1..3] (0: 종료)" );
     
-      int menuNo = keyboardInput.nextInt();
-      keyboardInput.nextLine(); //입력한 
+      int menuNo = keyboardInput.nextInt(); //키보드에서 입력할 때까지 대기 == 블로킹
+      keyboardInput.nextLine(); //입력한 숫자 뒤에 남아 있는 줄바꿈(엔터) 코드 제거
 
      if (menuNo == 0) {
-        break;
+        break; //반복문이나 swich문을 만날 때까지 블록 탈출 if문 아님!
      } else if (menuNo == 1) {
-        System.out.println("게시글 목록");  
+        System.out.println("[게시글 목록]");  
+
         System.out.println("번호 제목 조회수 작성자 등록일");
         System.out.print(1);
         System.out.print("\t");
@@ -45,18 +56,33 @@ public class App {
 
 
       } else if (menuNo == 2) {
-        System.out.println("게시글 상세보기");
+        System.out.println("[게시글 상세보기]");
+
         System.out.printf("번호: %d\n", 1);
-        System.out.printf("제목: %s\n", "제목입니다1");
-        System.out.printf("내용: %s\n", "내용입니다");
+        System.out.printf("제목: %s\n", title);
+        System.out.printf("내용: %s\n", content);
         System.out.printf("조회수: %d\n", 100);
-        System.out.printf("작성자: %s\n", "홍길동");
+        System.out.printf("작성자: %s\n", writer);
         System.out.printf("등록일: %s\n", "2022-07-08");
         System.out.println();
-        System.out.print("메뉴를 선택하세요[1..2] (0: 종료) ");
+      
+      } else if (menuNo == 3) {
+        System.out.println("[게시글 등록]");
+        
+        System.out.print("제목?");
+        title = keyboardInput.nextLine();
+        System.out.print("내용?");
+        content = keyboardInput.nextLine();
+        System.out.print("작성자?");
+        writer = keyboardInput.nextLine();
+        System.out.print("암호?");
+        password = keyboardInput.nextLine();
+
         } else {
           System.out.println("메뉴 번호가 옳지 않습니다!");
         }
+        
+        System.out.println(); // 메뉴를 처리한 후 빈 줄 출력
     } //while
 
       System.out.println("안녕히 가세요!");
