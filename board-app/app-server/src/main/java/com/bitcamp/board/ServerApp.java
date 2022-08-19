30f6788fff8b246280337a5db737b5e4c9bea431
zzallang <wmfsp9@naver.com>
2022-08-19 16:22:01 +0900

    044.  통신 방식을 Stateful에서 Stateless로 변경하기 1.

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -21,23 +21,22 @@ public class ServerApp {
       System.out.println("서버 소켓 준비 완료!");
       System.out.println();
 
-      try (Socket socket = serverSocket.accept(); // 서버 대기 중,, 
+      // 클라이언트 요청을 처리할 객체 준비
+      Hashtable<String,Servlet> servletMap = new Hashtable<>();
+      servletMap.put("board",new BoardServlet("board"));
+      servletMap.put("reading",new BoardServlet("reading"));
+      servletMap.put("visit",new BoardServlet("visit"));
+      servletMap.put("notice",new BoardServlet("notice"));
+      servletMap.put("diary",new BoardServlet("diary"));
+      servletMap.put("member",new MemberServlet("member"));
 
-          DataInputStream in = new DataInputStream(socket.getInputStream());
-          DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+      while (true) {
+        try (Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
 
-        System.out.println("클라이언트와 연결 되었음!");
+          System.out.println("클라이언트와 연결 되었음!");
 
-        // 클라이언트 요청을 처리할 객체 준비
-        Hashtable<String,Servlet> servletMap = new Hashtable<>();
-        servletMap.put("board",new BoardServlet("board"));
-        servletMap.put("reading",new BoardServlet("reading"));
-        servletMap.put("visit",new BoardServlet("visit"));
-        servletMap.put("notice",new BoardServlet("notice"));
-        servletMap.put("diary",new BoardServlet("diary"));
-        servletMap.put("member",new MemberServlet("member"));
-
-        while (true) {
           // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
           String dataName = in.readUTF();
 
@@ -47,21 +46,18 @@ public class ServerApp {
 
           Servlet servlet = servletMap.get(dataName);
           if (servlet != null) {
-            servlet.service(in,out);
+            servlet.service(in, out);
           } else {
             out.writeUTF("fail");
           }
-        }
 
-        System.out.println("클라이언트와 연결을 끊었음!");
-      } // 입출력의 자동 close()를 자동 호출하기 위한 안 쪽 try
-
+          System.out.println("클라이언트와 연결을 끊었음!");
+        } // 안쪽 try
+      }
     } catch (Exception e) {
       e.printStackTrace();
-    } // 바깥 쪽 try
+    } // 바깥 쪽 try 
 
     System.out.println("서버 종료!");
   }
 }
-
-
