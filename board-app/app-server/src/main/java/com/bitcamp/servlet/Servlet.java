e3c30032184aa7724964e35195ac94a4b0d5c822
zzallang <wmfsp9@naver.com>
2022-08-19 14:57:13 +0900

    043.  Client / Server 공통 코드를 라이브러리 프로젝트로 분리 1.

--- board-app/app-server/src/main/java/com/bitcamp/servlet/Servlet.java
+++ board-app/app-server/src/main/java/com/bitcamp/servlet/Servlet.java
@@ -6,7 +6,8 @@ import java.io.DataOutputStream;
 // 사용자 요청을 다룰 객체의 사용법을 정의한다.
 //
 public interface Servlet {
-  String SUCCESS = "success"; 
-  String FAIL = "fail"; // 인터페이스에 선언된 필드는 무조건 public static final임 생략 가능
+  String SUCCESS = "success";
+  String FAIL = "fail";
+
   void service(DataInputStream in, DataOutputStream out);
 }
