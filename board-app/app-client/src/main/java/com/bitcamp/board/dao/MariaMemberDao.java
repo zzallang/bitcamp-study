c2b8dcaceb48278bd3fb4a2f067951e2ab47786f
zzallang <wmfsp9@naver.com>
2022-09-07 11:35:51 +0900

    DBMS 도입하기 3

---
+++ board-app/app-client/src/main/java/com/bitcamp/board/dao/MariaMemberDao.java
@@ -0,0 +1,75 @@
+package com.bitcamp.board.dao;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import com.bitcamp.board.domain.Member;
+import com.google.gson.Gson;
+
+public class MariaMemberDao {
+
+  public boolean insert(Member member) throws Exception {
+    try (Socket socket = new Socket(ip,port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+      out.writeUTF(dataName);
+      out.writeUTF("insert");
+      out.writeUTF(new Gson().toJson(member));
+      return in.readUTF().equals("success");
+    }
+  }
+
+  public boolean update(Member member) throws Exception {
+    try (Socket socket = new Socket(ip,port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+      out.writeUTF(dataName);
+      out.writeUTF("update");
+      out.writeUTF(new Gson().toJson(member));
+      return in.readUTF().equals("success");
+    }
+  }
+
+  public Member findByEmail(String email) throws Exception {
+    try (Socket socket = new Socket(ip,port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+      out.writeUTF(dataName);
+      out.writeUTF("findByEmail");
+
+      if (in.readUTF().equals("fail")) {
+        return null;
+      }
+      return new Gson().fromJson(in.readUTF(), Member.class);
+    }
+  }
+
+  public boolean delete(String email) throws Exception {
+    try (Socket socket = new Socket(ip,port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+      out.writeUTF(dataName);
+      out.writeUTF("delete");
+      return in.readUTF().equals("success");
+    }
+  }
+
+  public Member[] findAll() throws Exception {
+    try (Socket socket = new Socket(ip,port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+      out.writeUTF(dataName);
+      out.writeUTF("findAll");
+
+      if (in.readUTF().equals("fail")) {
+        return null;
+      }
+      return new Gson().fromJson(in.readUTF(), Member[].class);
+    }
+  }
+}
