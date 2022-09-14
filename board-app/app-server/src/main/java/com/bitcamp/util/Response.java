599f934c20302983ee9ef5515a6fb70bee834b79
zzallang <wmfsp9@naver.com>
2022-09-14 15:32:32 +0900

    리팩토링

---
+++ board-app/app-server/src/main/java/com/bitcamp/util/Response.java
@@ -0,0 +1,11 @@
+package com.bitcamp.util;
+
+import java.io.DataOutputStream;
+
+public class Response {
+
+  public void sendMessage(DataOutputStream out, String message) throws Exception {
+
+
+  }
+}
