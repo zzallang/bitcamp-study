67377107c71bd444b6779881e3e7ac733edcc789
zzallang <wmfsp9@naver.com>
2022-09-15 12:04:36 +0900

    Mini Web Server02

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -3,6 +3,7 @@ package com.bitcamp.board;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -11,36 +12,32 @@ public class MiniWebServer {
 
   public static void main(String[] args) throws Exception {
 
-    // 클라이언트 요청을 처리하는 객체
     class MyHttpHandler implements HttpHandler {
       @Override
       public void handle(HttpExchange exchange) throws IOException {
-        // 클라이언트 요청이 들어왔을 때 마다 호출된다.
         System.out.println("클라이언트가 요청함!");
 
-        // 응답할 콘텐트 준비
-        String response = "This is the response";
+        String response = "ABCabc123가각간";
+        byte[] bytes = response.getBytes("UTF-8");
 
-        // 응답헤더 전송
-        exchange.sendResponseHeaders(200, response.length());
+        // 보내는 컨텐트의 MIME 타입이 무엇인지 응답 헤더에 추가한다.
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.add("Content-Type", "text/plain; charset=UTF-8");
 
-        // 콘텐트 출력 스트림 준비
+        exchange.sendResponseHeaders(200, bytes.length);
+
         OutputStream out = exchange.getResponseBody();
-
-        out.write(response.getBytes());
+        out.write(bytes);
         out.close();
       }
     }
 
     HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
     server.createContext("/", new MyHttpHandler());
-    server.setExecutor(null); // HttpServer에 기본으로 설정되어있는 Executor 사용!
-    // Executor? 멀티 스레딩을 수행하는 객체
-    server.start(); // HttpServer를 시작시킨 후 즉시 리턴한다.
+    server.setExecutor(null); 
+    server.start();
 
     System.out.println("서버 시작!");
-    // main() 메소드 호출이 끝났다 하더라고
-    // 내부에서 생성한 스레드 (예 : HttpServer)가 종료되지 않으면 JVM도 종료되지 않는다.
   }
 
 }
