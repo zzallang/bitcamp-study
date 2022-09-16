7096c24baf8db9ed0f3896c31a92017d2afa7ac8
zzallang <wmfsp9@naver.com>
2022-09-16 12:12:45 +0900

    Mini Web Server07

--- board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/MiniWebServer.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.HashMap;
@@ -45,7 +46,8 @@ public class MiniWebServer {
 
         URI requestUri = exchange.getRequestURI();
         String path = requestUri.getPath();
-        String query = requestUri.getQuery();
+        //        String query = requestUri.getQuery(); // 디코딩을 제대로 수행하지 못한다!
+        String query = requestUri.getRawQuery(); // 디코딩 없이 query string을 그대로 리턴 받기!
         byte[] bytes = null;
 
         try (StringWriter stringWriter = new StringWriter();
@@ -56,11 +58,13 @@ public class MiniWebServer {
             String[] entries = query.split("&");
             for (String entry : entries) { // 예) no=1
               String[] kv = entry.split("=");
-              paramMap.put(kv[0], kv[1]);
+              // 웹브라우저가 보낸 파라미터 값은 저장하기 전에 URL 디코딩한다.
+              paramMap.put(kv[0], URLDecoder.decode(kv[1],"UTF-8"));
             }
           }
 
           System.out.println(path);
+          System.out.println(query);
           System.out.println(paramMap);
 
           if (path.equals("/")) {
