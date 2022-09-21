dc135be4f525e8fc7321dd22738794a3a1e8bceb
zzallang <wmfsp9@naver.com>
2022-09-21 16:24:52 +0900

    app-58 서블릿이 사용할 자원 준비하기 1

--- board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
@@ -2,16 +2,39 @@ package com.bitcamp.board.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.bitcamp.board.dao.BoardDao;
+import com.bitcamp.board.dao.MariaDBBoardDao;
+import com.bitcamp.board.dao.MariaDBMemberDao;
+import com.bitcamp.board.dao.MemberDao;
 
-@WebServlet(value="/welcome3")
-public class Welcome3Servlet extends HttpServlet{
+/**
+ * 이 서블릿은 다른 서블릿이 사용할 객체를 준비하는 일을 한다.
+ */
+@WebServlet(value="/init")
+public class AppInitServlet extends HttpServlet{
 
   private static final long serialVersionUID = 1L;
+
+  public static BoardDao boardDao;
+  public static MemberDao memberDao;
+
+  public AppInitServlet() throws Exception {
+    System.out.println("공유 자원을 준비 중!");
+
+    Class.forName("org.mariadb.jdbc.Driver");
+
+    Connection con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/studydb","study","1111");
+    boardDao = new MariaDBBoardDao(con);
+    memberDao = new MariaDBMemberDao(con);
+  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse res)
@@ -29,12 +52,7 @@ public class Welcome3Servlet extends HttpServlet{
     out.println("<title>bitcamp</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>환영합니다!3</h1>");
-    out.println("<p>비트캠프 게시판 관리 시스템 프로젝트입니다.</p>");
-    out.println("<ul>");
-    out.println("  <li><a href='/board/list'>게시글</a></li>");
-    out.println("  <li><a href='/member/list'>회원</a></li>");
-    out.println("</ul>");
+    out.println("<h1>웹 애플리케이션 준비!</h1>");
     out.println("</body>");
     out.println("</html>");
   }
