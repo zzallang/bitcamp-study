95bdc40bfbadafb6ebe84527eb8a4ce74c9bd577
zzallang <wmfsp9@naver.com>
2022-09-22 11:36:48 +0900

    app-59 공동 보관소를 사용하여 서블릿이 사용할 지언을 공유하기 1

--- board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/servlet/AppInitServlet.java
@@ -22,8 +22,6 @@ public class AppInitServlet extends HttpServlet{
   public void init() throws ServletException {
     // 톰캣서버 --> init(ServletConfig) --> init()
 
-    System.out.println("공유 자원을 준비 중!");
-
     try {
       Class.forName("org.mariadb.jdbc.Driver");
 
@@ -31,8 +29,11 @@ public class AppInitServlet extends HttpServlet{
           "jdbc:mariadb://localhost:3306/studydb","study","1111");
 
       // 생성자에서 getServetContext()를 호출하면 오류 발생!
-      // 왜? 아직 ServletContext 객체가 준비되지 않았기 때문이다.
-      // 그래서 생성자 다음에 호출되는 init()에서 getServletContext()를 호출해야 한다.
+      // 왜? 
+      //  -ServletContext()는 ServletConfig 객체를 통해 꺼낼 수가 있는데,
+      //  -ServletConfig는 아직 주입되지 않은 상태이다.
+      // ServletConfig 객체가 언제 주입되는가?
+      //  - 생성자 다음에 호출되는 init()가 호출될 때 ServletConfig 객체가 주입된다.
       //
       ServletContext ctx = this.getServletContext();
       ctx.setAttribute("boardDao", new MariaDBBoardDao(con));
