89c286588cbb8dcb320f179af04b3c167f8d3c09
zzallang <wmfsp9@naver.com>
2022-10-07 11:16:13 +0900

    app-77 Front Controller 디자인 패턴 도입하기 2

--- board-app/app-server/src/main/java/com/bitcamp/servlet/DispatcherServlet.java
+++ board-app/app-server/src/main/java/com/bitcamp/servlet/DispatcherServlet.java
@@ -2,7 +2,6 @@ package com.bitcamp.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 // 클라이언트가 요청한 경로에 따라 적절한 페이지 컨트롤러를 실행한다.
 // 페이지 컨트롤러의 공통 기능을 수행한다.
 //
-@WebServlet(value = "/service/*")
+//@WebServlet(value = "/service/*")
 public class DispatcherServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
