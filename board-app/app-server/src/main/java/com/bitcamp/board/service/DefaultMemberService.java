b3eed1ca71c39f800a993fc6d76a080be7968b36
zzallang <wmfsp9@naver.com>
2022-10-11 14:52:54 +0900

    app-79 Spring IoC 컨테이너 도입하기: 페이지 컨트롤러 생성 7

--- board-app/app-server/src/main/java/com/bitcamp/board/service/DefaultMemberService.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/service/DefaultMemberService.java
@@ -1,17 +1,20 @@
 package com.bitcamp.board.service;
 
 import java.util.List;
+import org.springframework.stereotype.Component;
 import com.bitcamp.board.dao.MemberDao;
 import com.bitcamp.board.domain.Member;
 
+@Component
+//- 이 애노테이션을 붙이면 Spring IoC 컨테이너가 객체를 자동으로 생성할 것이다.
+//- 생성자에 파라미터가 있다면 해당 타입의 객체를 찾아 생성자를 호출할 때 주입할 것이다.
+//- 만약 생성자가 원하는 파라미터 값이 없다면 생성 예외가 발생한다.
 public class DefaultMemberService implements MemberService {
 
-  // 비즈니스 로직을 수행하는 객체
-  // - 메소드의 이름은 업무와 관련된 이름을 사용한다.
-  // 
   MemberDao memberDao;
 
   public DefaultMemberService(MemberDao memberDao) {
+    System.out.println("DefaultMemberService() 호출됨!");
     this.memberDao = memberDao;
   }
 
