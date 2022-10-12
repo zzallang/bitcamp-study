5732c7e407df7422b2682be194dde0335dd300f2
zzallang <wmfsp9@naver.com>
2022-10-12 14:13:12 +0900

    app-82 Spring WebMVC 프레임워크 사용법 2

--- board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/listener/ContextLoaderListener.java
@@ -1,5 +1,8 @@
 package com.bitcamp.board.listener;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -8,8 +11,11 @@ import javax.servlet.ServletRegistration.Dynamic;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import com.bitcamp.board.config.AppConfig;
+import com.bitcamp.board.filter.AdminCheckFilter;
+import com.bitcamp.board.filter.LoginCheckFilter;
 
 // 웹애플리케이션이 시작되었을 때 공유할 자원을 준비시키거나 해제하는 일을 한다.
 //
@@ -35,6 +41,28 @@ public class ContextLoaderListener implements ServletContextListener {
       config.setMultipartConfig(new MultipartConfigElement(
           this.getClass().getAnnotation(MultipartConfig.class)));
       config.setLoadOnStartup(1); // 웹 애플리케이션을 시작할 때 프론트 컨트롤러를 자동 생성.
+
+      // 필터 등록
+      CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8");
+      FilterRegistration.Dynamic filterConfig = ctx.addFilter("CharacterEncodingFilter", filter); // 필터를 설정하는 설정 객체를 리턴
+      filterConfig.addMappingForServletNames( // 설정 객체에 서블릿 필터로 연결하겠다.
+          EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),
+          false, // 서블릿을 사용하기 전에 이 필터를 적용하라
+          "DispatcherServlet"); // 필터 이름을 적용했기 때문에 다른 서블릿은 안됨!
+
+      AdminCheckFilter adminFilter = new AdminCheckFilter();
+      FilterRegistration.Dynamic adminFilterConfig = ctx.addFilter("AdminCheckFilter", adminFilter);
+      adminFilterConfig.addMappingForUrlPatterns(
+          EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),
+          false,
+          "/service/member/*");
+
+      LoginCheckFilter loginFilter = new LoginCheckFilter();
+      FilterRegistration.Dynamic loginFilterConfig = ctx.addFilter("LoginCheckFilter", loginFilter);
+      loginFilterConfig.addMappingForUrlPatterns(
+          EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),
+          false,
+          "/service/*");
 
     } catch (Exception e) {
       e.printStackTrace();
